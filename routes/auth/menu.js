6856b15b2e95825109293ac803b7061c32e78674
lambaovuong_12a1@yahoo.com <lambaovuong_12a1@yahoo.com>
2018-06-14 16:20:10 +0700

    update service for menu

--- routes/auth/menu.js
+++ routes/auth/menu.js
@@ -1,14 +1,43 @@
 var express = require('express');
 var mongoose = require('mongoose');
+const _ = require('lodash');
+
 var router = express.Router();
 
 const Menus = require('../../Schema/Auth/Menu');
+const AccessLink = require('../../Schema/Auth/AccessLink');
+
+findMenuNameById = (menu_id) => {
+    return Menus.find({ _id: new mongoose.mongo.ObjectID(menu_id) });
+}
+
+findAccessLinkNameById = (access_link_id) => {
+    return AccessLink.find({ _id: new mongoose.mongo.ObjectID(access_link_id) });
+}
+
+findMenuChildsByParent = (parent_id) => {
+    return Menus.find({ menu_parent_id: new mongoose.mongo.ObjectID(parent_id) });
+}
+
+copyMenuData = (mi) => {
+    let item = {
+        menu_label: mi.menu_label,
+        menu_parent_id: mi.menu_parent_id,
+        access_link_id: mi.access_link_id,
+        create_date: mi.create_date,
+        update_date: mi.update_date,
+        record_status: mi.record_status,
+        _id: mi._id,
+        __v: mi.__v
+    };
+    return item;
+}
 
 router.get('/get', (req, res, next) => {
     req.query.record_status = 'O';
-    let cond ={...req.query };
-    if( cond.menu_parent){
-        cond.menu_parent_id=new mongoose.Types.ObjectId(cond.menu_parent);
+    let cond = { ...req.query };
+    if (cond.menu_parent) {
+        cond.menu_parent_id = new mongoose.Types.ObjectId(cond.menu_parent);
         delete cond.menu_parent;
     }
     console.log(cond);
@@ -22,6 +51,78 @@ router.get('/get', (req, res, next) => {
         })
 })
 
+router.get('/getwithlabels', (req, res, next) => {
+    req.query.record_status = 'O';
+    let cond = { ...req.query };
+    if (cond.menu_parent) {
+        cond.menu_parent_id = new mongoose.Types.ObjectId(cond.menu_parent);
+        delete cond.menu_parent;
+    }
+    Menus.find(cond)
+        .sort({ 'menu_label': 'asc' })
+        .exec(async (err, ms) => {
+            if (!err) {
+                let data_returned = [];
+                for (let i = 0; i < ms.length; i++) {
+                    let item = copyMenuData(ms[i]);
+                    if (item.menu_parent_id) {
+                        let findMenuParent = await findMenuNameById(item.menu_parent_id);
+                        if (findMenuParent.length > 0) { item.menu_parent_label = findMenuParent[0].menu_label; }
+                    }
+                    if (item.access_link_id) {
+                        let findAccessLink = await findAccessLinkNameById(item.access_link_id);
+                        if (findAccessLink.length > 0) { item.access_link_name = findAccessLink[0].name; }
+                    }
+                    data_returned.push(item);
+                }
+                data_returned.sort((a, b) => {
+                   // console.log('a->'+"a".localeCompare('b'));
+                   let a_p_label =a.menu_parent_label+"";
+                   let b_p_label =b.menu_parent_label+"";
+                    if (a_p_label.localeCompare(b_p_label) === 1) { return -1; }
+                    else if (a_p_label.localeCompare(b_p_label) === 0) {
+                        let a_label=a.menu_label+"";
+                        let b_label=b.menu_label+"";
+                        return (a_label.localeCompare(b_label));
+                    }
+                    return 1;
+                });
+
+                return res.status(200).send({ valid: true, data: data_returned });
+            }
+            return res.status(200).send({ valid: false, message: err });
+        })
+})
+
+router.get('/getroot', (req, res, next) => {
+    //req.query.record_status = 'O';
+    let cond = { record_status: 'O', menu_parent_id: null };
+    console.log(cond);
+    Menus.find(cond)
+        .sort({ 'menu_label': 'asc' })
+        .exec(async (err, ms) => {
+            if (!err) {
+                let data_return = [];
+                for (let i = 0; i < ms.length; i++) {
+                    let item = copyMenuData(ms[i]);
+                    let findChilds = await findMenuChildsByParent(item._id);
+                    // /children
+                    let child = [];
+                    for (let j = 0; j < findChilds.length; j++) {
+                        let ch = copyMenuData(findChilds[j]);
+                        ch.key = ch._id;
+                        ch.title = ch.menu_label;
+                        child.push(ch);
+                    }
+                    item.children = child;
+                    data_return.push(item);
+
+                }
+                return res.status(200).send({ valid: true, data: data_return });
+            }
+            return res.status(200).send({ valid: false, message: ms });
+        })
+})
 
 router.post('/add/', (req, res, next) => {
     console.log(req.body);
@@ -34,10 +135,10 @@ router.post('/add/', (req, res, next) => {
         return res.status(200).send({ valid: false, message: ex });
     }
 
-    if(req.body.menu_parent){
-        try{
+    if (req.body.menu_parent) {
+        try {
             ms.menu_parent_id = new mongoose.Types.ObjectId(req.body.menu_parent);
-        }catch(ex){
+        } catch (ex) {
             console.log(ex);
             return res.status(200).send({ valid: false, message: ex });
         }
