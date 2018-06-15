cf66409a2c35cecfa745f607eef93d62fe0e1c01
Minh Tam <minhtam@SVR3.local>
2018-06-15 14:17:20 +0700

    cập nhật menu-role

--- Schema/Auth/Role.js
+++ Schema/Auth/Role.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const role = new mongoose.Schema({
     role_name: {type: String, default: null},
+    menu: [{type: mongoose.Schema.Types.ObjectId, ref: 'menus'}],
     create_date: {type: Date, default: new Date()},
     update_date: {type: Date, default: null},
     record_status: {type: String, default: 'O'}
