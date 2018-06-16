5ea2582fb9083d5b7f1dc51a380aaa0bd00a2430
lambaovuong_12a1@yahoo.com <lambaovuong_12a1@yahoo.com>
2018-06-16 07:30:49 +0700

    dev

--- routes/auth/accesslink.js
+++ routes/auth/accesslink.js
@@ -5,7 +5,7 @@ var router = express.Router();
 const AccessLink = require('../../Schema/Auth/AccessLink');
 
 router.get('/get', (req, res, next) => {
-    req.query.record_status = 'O';
+   // req.query.record_status = 'O';
     console.log(req.query);
     AccessLink.find(req.query)
         .sort({ 'name': 'asc' })
@@ -55,4 +55,47 @@ router.post(`/update/:id/`, (req, res, next) => {
     })
 });
 
+
+router.post(`/enable/:id/`, (req, res, next) => {
+    console.log(req.params);
+    let cond ={
+        _id: req.params.id,
+        __v: req.body.v
+    }
+    console.log('enable with conditions  ==>' + JSON.stringify(cond));
+    let data = {
+        update_date: new Date(),
+        record_status:'O',
+        $inc: { __v: 1 }
+    };
+    AccessLink.findByIdAndUpdate(cond, data, (err, data) => {
+        if (!err) {
+            return res.status(200).send({ valid: true, data: data });
+        }
+        return res.status(200).send({ valid: false, message: err });
+
+    })
+});
+
+
+router.post(`/disable/:id/`, (req, res, next) => {
+    console.log(req.params);
+    let cond ={
+        _id: req.params.id,
+        __v: req.body.v
+    }
+    console.log('enable with conditions  ==>' + JSON.stringify(cond));
+    let data = {
+        update_date: new Date(),
+        record_status:'C',
+        $inc: { __v: 1 }
+    };
+    AccessLink.findByIdAndUpdate(cond, data, (err, data) => {
+        if (!err) {
+            return res.status(200).send({ valid: true, data: data });
+        }
+        return res.status(200).send({ valid: false, message: err });
+
+    })
+});
 module.exports = router;
