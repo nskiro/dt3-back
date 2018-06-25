e25f224e4fa2874662afbd35deac822c85bfe45d
Minh Tam <minhtam@SVR3.local>
2018-06-25 08:20:23 +0700

    add đệ quy menu

--- routes/auth.js
+++ routes/auth.js
@@ -2,30 +2,30 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('../helper');
 var _ = require('lodash');
-var att = require('list-to-tree-lite');
 const user = require('../Schema/Auth/User');
 const menu = require('../Schema/Auth/Menu');
 
+dequy = (data, parentId = null) => {
+    let tempArr = [];
+    _.forEach(data, (item) => {
+        if (String(item.menu_parent_id) === String(parentId)) {
+            const arrItem = {...item};
+            tempArr.push(arrItem._doc);
+        }
+    });
+
+    if (tempArr.length > 0) {
+        return tempArr.map((item) => {
+            return { ...item, children: dequy(data, item._id) };
+        });
+    }
+}
+
 router.post('/login', (req, res, next) => {
-    // user.findOne({ username: req.body.username, password: req.body.password }, (err, objUser) => {
-    //     if (err) {
-    //         return res.status(500).send(err);
-    //     }
-    //     if (objUser) {
-    //         const tokenStr = jwt.makeToken(objUser);
-    //         const resInfo = {
-    //             token: tokenStr
-    //         }
-    //         return res.status(200).json(resInfo);
-    //     }
-    //     else {
-    //         return res.status(500).send("Error: User not found, please try again");
-    //     }
-    // });
     user.findOne({ username: req.body.username, password: req.body.password, record_status: 'O' })
         .populate({ path: 'group', match: { record_status: 'O' }, populate: { path: 'role', match: { record_status: 'O' }, populate: { path: 'menu', match: { record_status: 'O' } } } })
         .populate({ path: 'role', match: { record_status: 'O' }, populate: { path: 'menu', match: { record_status: 'O' } } })
-        .exec(async (err, doc) => {
+        .exec((err, doc) => {
             if (!err) {
                 const groups = doc.group.map((group) => {
                     return group.group_name;
@@ -56,17 +56,17 @@ router.post('/login', (req, res, next) => {
                             userMenu.push(menu);
                         })
                     })
-                })
-                menu.find({ record_status: 'O' }, (err, docs) => {
-                    return res.status(200).send(docs);
                 });
 
-                // let resData = { ...doc };
-                // resData._doc.group = groups;
-                // resData._doc.role = _.uniq(_.union(roles, subRoles));
-                // resData._doc.menu = menus;
+                menu.find({ record_status: 'O' }, (err, docs) => {
+                    const jsonMenu = dequy(docs);
+                    let resData = { ...doc };
+                    resData._doc.group = groups;
+                    resData._doc.role = _.uniq(_.union(roles, subRoles));
+                    resData._doc.menu = jsonMenu;
 
-                // return res.status(200).send(resData._doc);
+                    return res.status(200).send(resData._doc);
+                });
             }
             //return res.status(500).send(err);
         });
