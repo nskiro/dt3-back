ba2ec17a493756de6a420dac372e1c37bbca1aad
vuonglb <lambaovuong_12a1@yahoo.com>
2018-07-12 15:48:40 +0700

    update view for test fabric relax

--- Schema/TestFabric/TestFabricRelax.js
+++ Schema/TestFabric/TestFabricRelax.js
@@ -7,12 +7,17 @@ const TestFabricRelax = new mongoose.Schema({
     condition_hours: { type: Number, default: null },
     note: { type: String, default: null },
 
+
     start_date: { type: Date, default: null },
     end_date: { type: Date, default: null },
+
+    fabric_relax_detail_id: [{ type: mongoose.Schema.Types.ObjectId, ref: 'testfabricrelaxdetails' }],
 
     create_date: { type: Date, default: null },
     update_date: { type: Date, default: null },
     record_status: { type: String, default: 'O' }
+
+
 });
 TestFabricRelax.virtual('id').get(function () {
     return this._id;
